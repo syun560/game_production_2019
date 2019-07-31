931788afeb1585e693a49841e894a88a1246c74d
syun560 <bajikoji@gmail.com>
2019-07-31 16:50:02 +0900

    Update README.md

--- Source.cpp
+++ Source.cpp
@@ -1,63 +1,21 @@
-#include "Dxlib.h"
-
-struct Ball {
-	int x, y, r, spd;
-	double vx, vy;
-};
+#include "global.h"
+#include "MainScene.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
 	// DXライブラリ初期化
 	ChangeWindowMode(TRUE);
+	SetGraphMode(FMX, FMY, 32);
 	if (DxLib_Init() == -1)	return -1;
 	SetDrawScreen(DX_SCREEN_BACK); // 裏画面化
 
-	// 変数宣言、初期化
-	Ball ball[50];
-	for (int i = 0; i < 50; i++) {
-		ball[i].x = GetRand(680);
-		ball[i].y = GetRand(400);
-		ball[i].vx = -3 + GetRand(5);
-		ball[i].vy = 4 + GetRand(2);
-		ball[i].spd = 3;
-		ball[i].r = 10;
-	}
-	int x = 0;
-	int y = 0;
-	int r = 50;
-	int spd = 5;
+	// 初期化
+	MainScene mainScene;
 
 	// メインループ（60FPSなら1秒間に60回実行される）
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
-		// 入力
-		char keyState[256];
-		GetHitKeyStateAll(keyState);
-
-
-		// 計算
-		if (keyState[KEY_INPUT_RIGHT] == 1) x += spd;
-		if (keyState[KEY_INPUT_LEFT] == 1) x -= spd;
-		if (keyState[KEY_INPUT_UP] == 1) y -= spd;
-		if (keyState[KEY_INPUT_DOWN] == 1) y += spd;
-
-		for (int i = 0; i < 50; i++) {
-			ball[i].vy += 0.1;
-			ball[i].x += (int)ball[i].vx;
-			ball[i].y += (int)ball[i].vy;
-			if (ball[i].x > 600) ball[i].vx *= -1.0;
-			if (ball[i].x < 0) ball[i].vx *= -1.0;
-			if (ball[i].y > 480) ball[i].vy *= -1;
-		}
-
-		if (keyState[KEY_INPUT_ESCAPE] == 1) break;
-
-		// 描画
-		DrawCircle(x, y, r, GetColor(122, 255, 255));
-		for (int i = 0; i < 50; i++) {
-			DrawCircle(ball[i].x, ball[i].y, ball[i].r, GetColor(255, 15, 255));
-		}
-
-		DrawFormatString(0, 0, GetColor(255, 255, 255), "x = %d, y = %d", x, y);
+		mainScene.update();
+		mainScene.draw();
 	}
 
 	// DXライブラリ終了処理
---
+++ global.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "DxLib.h"
+
+const int FMX = 800;
+const int FMY = 600;
+
+const int WHITE = GetColor(255, 255, 255);
